2824bcdb2a83811d9f967fe17a35b05618270781
Ryan <ryfly2016@gmail.com>
2025-04-08 10:28:12 -0600

    create components

---
+++ src/components/Blue.jsx
@@ -0,0 +1,7 @@
+export function Blue() {
+  return (
+    <div className="blue">
+      <h1>blue</h1>
+    </div>
+  );
+}
---
+++ src/components/Red.jsx
@@ -0,0 +1,7 @@
+export function Red() {
+  return (
+    <div className="red">
+      <h1>red</h1>
+    </div>
+  );
+}
